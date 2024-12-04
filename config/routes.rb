1740100e9fc990df630bdcd115b70d5bfb9d49da
Joaquin Bernardini <bernardinijoaquin@gmail.com>
2024-12-04 21:29:16 +0000

    arreglos de la buy form

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "products#index"
-  resources :products do
-    resources :sales, only: [:new, :create]
-  end
+  resources :products
+  resources :sales
 end
