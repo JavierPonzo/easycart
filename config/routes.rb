c2fd470e6f44174a59eafb372b198b86de1587f6
Joaquin Bernardini <bernardinijoaquin@gmail.com>
2024-12-05 19:27:14 +0000

    add y vista my_products

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "products#index"
   get 'about', to: 'pages#about'
-  get 'my_products', to: 'pages#my_products'
+  get 'my_products', to: 'products#my_products'
   get 'confirm', to: 'pages#confirm', as: :confirm
   resources :products
   resources :sales
