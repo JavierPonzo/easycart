5bb2569506ddb1ae672df4369dccfd34cf425d50
Javier Ponzo <ponzojavier@gmail.com>
2025-01-14 21:00:09 +0100

    validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true, uniqueness: true
 end
